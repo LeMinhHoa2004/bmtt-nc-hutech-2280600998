20b196c844ead6595462e0e940f12099913777bc
LeMinhHoa2004 <zzztheold@gmail.com>
2025-03-04 10:11:58 +0700

    [add] lab-01 ex01 ex02

---
+++ lab-01/ex02/ex02_02.py
@@ -0,0 +1,6 @@
+#Nhap ban kinh tu nguoi dung
+ban_kinh=float(input("Nhap ban kinh cua hinh tron: "))
+#Tinh dien tich cua hinh tron
+dien_tich=3.14*(ban_kinh**2)
+#in dien tich cua hinh tron ra man hinh
+print("Dien tich hinh tron la:",dien_tich)
